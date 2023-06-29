1ceff2badf941534dd9f0adbbe7b24b156f8c25d
761579 <anjalimehra728@gmail.com>
2023-06-29 18:34:33 -0500

    routing

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import { Route, BrowserRouter as Router, Routes, } from 'react-router-dom';
-import Home from './Components/Home';
-import Education from './Components/Education';
-import Experience from './Components/Experience';
-import Projects from './Components/Projects';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+
+import { render } from 'react-dom';
+import Home from './Components/Home.js';
+import Education from './Components/Education.js';
+
 
 
 
@@ -12,15 +13,15 @@ import Projects from './Components/Projects';
 function App() {
   return (
     <div className="App">
+     
+    <Router>
+      <Routes>
+      
+      <Route path="/potfolio" element={<Navigate to="/home" replace />}/>
+       <Route  path='/home' element={<Home/>} />
+      </Routes>
+    </Router>
    
-     <Router>
- 
-     <Routes>
-    
-         <Route  path="/" element={<Home/>}/>
-         <Route path="/education"   Component={Education}/>
-    </Routes>
-     </Router>
     </div>
   );
 };
